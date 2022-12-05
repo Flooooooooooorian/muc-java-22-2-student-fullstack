d317fdf2a5f697858b698f271c3f371c23bb5f38
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-12-05 14:30:33 +0100

    Add Password Encoder

--- backend/src/main/java/de/neuefische/spring_request_params/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/spring_request_params/security/SecurityConfig.java
@@ -3,10 +3,9 @@ package de.neuefische.spring_request_params.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,7 +13,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new Argon2PasswordEncoder();
     }
 
     @Bean
