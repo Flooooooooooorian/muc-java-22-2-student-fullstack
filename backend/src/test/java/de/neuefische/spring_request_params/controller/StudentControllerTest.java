72af15318a1eafc68a1d3a9b30dcc58f0a47ddda
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-12-07 09:48:04 +0100

    Add Security Integration Tests

--- backend/src/test/java/de/neuefische/spring_request_params/controller/StudentControllerTest.java
+++ backend/src/test/java/de/neuefische/spring_request_params/controller/StudentControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -34,6 +35,7 @@ class StudentControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser
     void getStudents() throws Exception {
 
         mockMvc.perform(get("/api/students"))
@@ -44,6 +46,7 @@ class StudentControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser
     void addStudent() throws Exception {
         MvcResult response = mockMvc.perform(post("/api/students")
                         .contentType(MediaType.APPLICATION_JSON)
---
+++ backend/src/test/java/de/neuefische/spring_request_params/security/UserControllerTest.java
@@ -0,0 +1,51 @@
+package de.neuefische.spring_request_params.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    void helloMe_expectUser() throws Exception {
+
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
+
+    @Test
+    void helloMe_expectAnonymousUser() throws Exception {
+
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("anonymousUser"));
+    }
+
+    @Test
+    @WithMockUser(username = "Florian", roles = "ADMIN")
+    void helloMe_expectMyUser() throws Exception {
+
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Florian"));
+
+    }
+}
